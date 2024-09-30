8f4b78cbbf90aa334b4d33dda95e283c4f022a1a
Febryan Riyadi <115878138+Brazer27@users.noreply.github.com>
2024-09-30 23:19:04 +0700

    Update ContactView.vue

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -8,88 +8,82 @@ export default {
           id: 1,
           name: 'LinkedIN',
           imageUrl: 'https://cdn-icons-png.flaticon.com/512/174/174857.png',
-          status: 'Intermediate'
+          status: 'null'
         },
         {
           id: 2,
           name: 'GitHub',
           imageUrl: 'https://cdn-icons-png.flaticon.com/512/25/25231.png',
-          status: 'Intermediate'
+          status: 'null'
         },                    
         {
           id: 3,
           name: 'E-mail',
           imageUrl: 'https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png',
-          status: 'Intermediate'
+          status: 'null'
         },
         {
           id: 4,
           name: 'Instagram',
           imageUrl: 'https://cdn-icons-png.freepik.com/256/15789/15789364.png?semt=ais_hybrid',
-          status: 'Intermediate'
+          status: 'null'
         }
       ],
     };
+  },
+  methods: {
+    redirectTo(name) {
+      let url = '';
+      if (name === 'LinkedIN') {
+        url = 'www.linkedin.com/in/febryan-riyadi-7ab340219';
+      } else if (name === 'GitHub') {
+        url = 'https://github.com/Brazer27';
+      } else if (name === 'E-mail') {
+        url = 'mailto:febryanriyadi03@gmail.com'; // Update to your actual email
+      } else if (name === 'Instagram') {
+        url = 'https://www.instagram.com/febryan_4123?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==';
+      }
+      window.open(url, '_blank');
+    }
   }
 }
 </script>
+
 <template>
   <div class="px-5 py-5 md:px-12 md:py-10 text-left text-amber-50 mx-3">
     <article data-page="about">
       <header>
         <div
           class="text-2xl font-bold text-white mb-10 fadein-bot title-section flex items-center justify-center flex-col">
-          <!-- <div class="h-[1px] w-10 bg-amber-200 md:w-20 aos-init aos-animate"></div> -->
           <h4>My Contacts</h4>
           <h4 class="text-base font-normal text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-amber-300">
             Feel free to contact me using the information below.</h4>
         </div>
       </header>
       <section>
-          <div class="grid grid-cols-2 gap-4 pb-32 md:grid-cols-3 md:gap-8 xl:grid-cols-4 xl:gap-10 2xl:gap-12">
-            <div v-for="item in tech" :key="item.id">
-              <div
-                class="item-tech flex cursor-pointer items-center gap-2 rounded border border-amber-200 px-2 py-2 hover:bg-amber-200 hover:bg-opacity-10 md:gap-3 lg:px-3">
-                <div class="flex h-12 w-12 items-center justify-center p-0 lg:h-16 lg:w-16 lg:p-2 zoom-in">
-                  <img alt="HTML" loading="lazy" width="32" height="32" decoding="async" data-nimg="1"
-                    class="img-tech drop-shadow-xl transition-all duration-300 h-[65%] w-[65%] lg:h-[85%] lg:w-[85%]  "
-                    :src="item.imageUrl" style="color: transparent;">
+        <div class="grid grid-cols-2 gap-4 pb-32 md:grid-cols-3 md:gap-8 xl:grid-cols-4 xl:gap-10 2xl:gap-12">
+          <div v-for="item in tech" :key="item.id" @click="redirectTo(item.name)">
+            <div
+              class="item-tech flex cursor-pointer items-center gap-2 rounded border border-amber-200 px-2 py-2 hover:bg-amber-200 hover:bg-opacity-10 md:gap-3 lg:px-3">
+              <div class="flex h-12 w-12 items-center justify-center p-0 lg:h-16 lg:w-16 lg:p-2 zoom-in">
+                <img alt="HTML" loading="lazy" width="32" height="32" decoding="async" data-nimg="1"
+                  class="img-tech drop-shadow-xl transition-all duration-300 h-[65%] w-[65%] lg:h-[85%] lg:w-[85%]"
+                  :src="item.imageUrl" style="color: transparent;">
+              </div>
+              <div class="flex items-center text-sm md:text-base lg:text-lg">
+                <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">
+                  {{ item.name }}
                 </div>
-                <div class="flex items-center text-sm md:text-base lg:text-lg">
-                  <div class="tech font-medium text-secondary transition-all duration-300 translate-y-0 ">{{ item.name }}
-                  </div>
-                  <div
-                    class="status-tech opacity-0 absolute mt-5 text-[10px] text-amber-200 transition-all duration-300 md:text-xs lg:text-sm">
-                    {{ item.status }}</div>
+                <div
+                  class="status-tech opacity-0 absolute mt-5 text-[10px] text-amber-200 transition-all duration-300 md:text-xs lg:text-sm">
+                  {{ item.status }}
                 </div>
               </div>
             </div>
           </div>
+        </div>
       </section>
-  </article>
-</div></template>
+    </article>
+  </div>
+</template>
 
-<style>
-.item-card:hover {
-  transition: transform 0.3s ease;
-  transform: translateY(-8px);
-}
-svg:hover{
-  stroke: #ffdb70;
-}
-@keyframes fadeZoomIn {
-  0% {
-    opacity: 0;
-    transform: scale(0.5);
-  }
-  100% {
-    opacity: 1;
-    transform: scale(1);
-  }
-}
-
-/* Menggunakan animasi pada elemen yang diinginkan */
-.fade-zoom-in {
-  animation: fadeZoomIn 1s ease-in-out;
-}
-</style>
