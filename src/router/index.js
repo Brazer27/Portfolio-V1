eb8427fd277331a1ff30cc3e3d976aaf127c53a5
Febryan Riyadi <115878138+Brazer27@users.noreply.github.com>
2024-10-01 11:42:21 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,24 @@ const routes = [
     name: 'home',
     component: HomeView,
     meta: { title: 'Home' }
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
+    meta: { title: 'About' }
+  },
+  {
+    path: '/portfolio',
+    name: 'portfolio',
+    component: () => import('../views/PortfolioView.vue'),
+    meta: { title: 'Portfolio' }
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: () => import('../views/ContactView.vue'),
+    meta: { title: 'Contact' }
   }
 ]
 
@@ -16,7 +34,7 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title + ' - febryanriyadi;';
+  document.title = to.meta.title + ' - febryanriyadi;'; // Mengambil judul dari meta atau mengatur judul default jika tidak ada
   next();
 });
 
